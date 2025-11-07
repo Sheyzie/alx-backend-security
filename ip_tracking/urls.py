f244bcb9c2f4e1c6346c6d67921ded9f8389d87d
Olukosi Oluwaseyi <oluwaseyiolukosi7@gmail.com>
2025-11-07 15:56:46 +0100

    Fix Bug: Could not resolve geolocation backend

--- ip_tracking/urls.py
+++ ip_tracking/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken import views
-from .views import RegisterView
+from .views import RegisterView, api_view
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('api-token-auth/', views.obtain_auth_token),
+    path('api-view/', api_view, name='api-view'),
 ]
